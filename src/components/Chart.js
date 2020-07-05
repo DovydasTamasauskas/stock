8560d676975ce17cf505dd10653b23acd731ad2e
dovydas <tamasauskas.dovydas@gmail.com>
2020-07-05 17:35:35 +0300

    added divedent

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -3,25 +3,36 @@ import Plot from 'react-plotly.js';
 import { CHART_URL, SMA_URL } from '../consts/CONST.js';
 import axios from 'axios';
 
-function Stock({symbol}) {
+function Stock({ symbol }) {
   const [Data, AddData] = useState([]);
   const [Price, AddPrice] = useState([]);
   const [SMA, AddSMA] = useState([]);
+  const [Bull, AddBull] = useState(0);
+  const [Bear, AddBear] = useState(0);
+  const [Divedent, AddDivedent] = useState(0);
 
   useEffect(() => {
+    //https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=TSLA&outputsize=compact&apikey=HGJWFG4N8AQ66ICD
     async function fetchData() {
       const result = await axios(
         CHART_URL.replace("__symbol__", symbol),
       );
+      let divident = 0;
       for (var key in result.data['Time Series (Daily)']) {
         AddPrice(Price => [...Price, result.data['Time Series (Daily)'][key]['1. open']]);
         AddData(Data => [...Data, key]);
+        divident = result.data['Time Series (Daily)'][key]['7. dividend amount']
+        if(divident != 0){ 
+          console.log(divident);
+          AddDivedent(Bull => Bull+parseFloat(divident));
+        }
       }
     }
     fetchData();
   }, [symbol]);
 
   useEffect(() => {
+    //https://www.alphavantage.co/query?function=SMA&symbol=TSLA&interval=daily&time_period=10&series_type=open&apikey=HGJWFG4N8AQ66ICD
     async function fetchData() {
       const result = await axios(
         SMA_URL.replace("__symbol__", symbol),
@@ -33,7 +44,19 @@ function Stock({symbol}) {
     fetchData();
   }, [symbol]);
 
+  useEffect(() => {
+    for (let i = 0; i < Price.length-1; i++){
+      if(Price[i] > Price[i+1]){
+        AddBull(Bull => Bull+1);
+      }else{
+        AddBear(Bear => Bear+1);
+      }
+    }
+  }, [Price.length == 100]);
+
   return (
+    <div>
+    <h1>{symbol}</h1>
       <Plot
         data={[
           {
@@ -49,8 +72,12 @@ function Stock({symbol}) {
             marker: {color: 'green'},
           }
         ]}
-        layout={{width: 720, height: 440, title: 'A Fancy Plot'}}
+        layout={{width: window.innerWidth, height: 440}}
       />
+      <div style={{color: "red"}}>Bullish {Bull}</div>
+      <div style={{color: "green"}}>Bearish {Bear}</div>
+      <div style={{color: "black"}}>Divedent {Divedent}</div>
+  </div>
   );
 }
 
