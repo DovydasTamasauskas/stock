866ea6561f8733dd6ab854cf3e0c81507cbf1148
dovydas <tamasauskas.dovydas@gmail.com>
2020-07-05 15:38:45 +0300

    added SMA

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,38 +1,52 @@
 import React, { useState, useEffect } from 'react';
 import Plot from 'react-plotly.js';
-import { API_Call } from '../consts/CONST.js';
+import { CHART_URL, SMA_URL } from '../consts/CONST.js';
+import axios from 'axios';
 
 function Stock({symbol}) {
-  const [X, AddX] = useState([]);
-  const [Y, AddY] = useState([]);
+  const [Data, AddData] = useState([]);
+  const [Price, AddPrice] = useState([]);
+  const [SMA, AddSMA] = useState([]);
 
   useEffect(() => {
-    //https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=TSLA&outputsize=compact&apikey=HGJWFG4N8AQ66ICD
-    fetch(API_Call.replace("__symbol__", symbol))
-    .then(
-      function(response) {
-        return response.json();
+    async function fetchData() {
+      const result = await axios(
+        CHART_URL.replace("__symbol__", symbol),
+      );
+      for (var key in result.data['Time Series (Daily)']) {
+        AddPrice(Price => [...Price, result.data['Time Series (Daily)'][key]['1. open']]);
+        AddData(Data => [...Data, key]);
       }
-    )
-    .then(
-      function(data) {
-        for (var key in data['Time Series (Daily)']) {
-          AddY(Y => [...Y, key]);
-          AddX(X => [...X, data['Time Series (Daily)'][key]['1. open']]);
-        }
+    }
+    fetchData();
+  }, [symbol]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const result = await axios(
+        SMA_URL.replace("__symbol__", symbol),
+      );
+      for (var key in result.data['Technical Analysis: SMA']) {
+        AddSMA(SMA => [...SMA, result.data['Technical Analysis: SMA'][key]['SMA']]);
       }
-    )
-  }, []);
+    }
+    fetchData();
+  }, [symbol]);
 
   return (
       <Plot
         data={[
           {
-            x: Y,
-            y: X,
+            x: Data,
+            y: Price,
             type: 'scatter',
-            mode: 'lines+markers',
             marker: {color: 'red'},
+          },
+          {
+            x: Data,
+            y: SMA,
+            type: 'scatter',
+            marker: {color: 'green'},
           }
         ]}
         layout={{width: 720, height: 440, title: 'A Fancy Plot'}}
--- src/consts/CONST.js
+++ src/consts/CONST.js
@@ -1,3 +1,6 @@
 
-const API_KEY = 'HGJWFG4N8AQ66ICD';
-export const API_Call = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=__symbol__&outputsize=compact&apikey=${API_KEY}`;
+const API_KEY = 'apikey=HGJWFG4N8AQ66ICD';
+const HOST = 'https://www.alphavantage.co/query';
+
+export const CHART_URL =       `${HOST}?function=TIME_SERIES_DAILY_ADJUSTED&symbol=__symbol__&outputsize=compact&${API_KEY}`;
+export const SMA_URL =         `${HOST}?function=SMA&symbol=__symbol__&interval=daily&time_period=10&series_type=open&${API_KEY}`;
