3384f20aad68234d681edc731dacc90121b01c9e
dovydas <tamasauskas.dovydas@gmail.com>
2020-07-08 21:31:54 +0300

    clean up backend

--- src/index.php
+++ src/index.php
@@ -2,7 +2,7 @@
     header('Access-Control-Allow-Origin: *');
     header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
     header('Access-Control-Allow-Headers: Content-Type, Accept');
-	$API_KEY = 'apikey=HGJWFG4N8AQ66ICD';
+	$API_KEY = 'apikey=ODCZDB41I8JYJNTV';
 	$HOST = 'https://www.alphavantage.co/';
 	$symbol = $_SERVER['QUERY_STRING'];
 	$pieces = explode(",", $symbol);
@@ -12,15 +12,21 @@
 		echo fread($myfile,filesize($pieces[1]."_".$pieces[2].".txt"));
 		fclose($myfile);
 	}else{
-		if($pieces[0] == 'Daily'){
-			$api_url = $HOST.'query?function=TIME_SERIES_DAILY_ADJUSTED&symbol='.$pieces[1].'&outputsize=compact&'.$API_KEY;
-		} else if ($pieces[0] == 'SMA') {
-			$api_url = $HOST.'query?function=SMA&symbol='.$pieces[1].'&interval=daily&time_period=10&series_type=open&'.$API_KEY;
-		}
-		
+		$api_url = getEndpiont($pieces[0], $pieces[1], $HOST, $API_KEY);
 		$xml = file_get_contents($api_url);
 		$myfile = fopen($pieces[0]."_".$pieces[1].".txt", "w") or die("Unable to open file!");
 		fwrite($myfile, $xml);
 		fclose($myfile);
+	}
+
+	function getEndpiont($action, $symbol, $HOST, $API_KEY){
+		switch ($action) {
+			case 'Daily':
+				return $HOST.'query?function=TIME_SERIES_DAILY_ADJUSTED&symbol='.$symbol.'&outputsize=compact&'.$API_KEY;
+				break;
+			case 'SMA':
+				return $HOST.'query?function=SMA&symbol='.$symbol.'&interval=daily&time_period=10&series_type=open&'.$API_KEY;
+				break;
+		}
 	}
 ?>
